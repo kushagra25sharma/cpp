072c1701a5f6383bc1f3a24782b8d016b011e7af
Kushagra Sharma <69106317+kushagra25sharma@users.noreply.github.com>
2021-05-17 10:24:45 +0530

    Point Update, Range Sum Query Binary Indexed Tree

---
+++ Fenwick Tree.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+//Point Update, Range Sum Query Binary Indexed Tree
+int n, BIT[100001], a[100001]; // 1 based indexing
+
+void build(int index, int num){
+    for(int i = index; i <= n; i += i&(-i)){
+        BIT[i] += num;
+    }
+}
+
+void update(int index, int num){
+    num -= a[index];
+    for(int i = index; i <= n; i += i&(-i)){
+        BIT[i] += num;
+    }
+}
+
+int query(int index){
+    ll sum = 0;
+    for(int i = index; i > 0; i -= i&(-i)){
+        sum += BIT[i];
+    }
+    return sum;
+}
+
+int main() {
+	ios_base :: sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	cin>>n;
+	for(int i=1;i<=n;i++){
+	    cin>>a[i];
+	    build(i, a[i]);
+	}
+	int q; // no of range sum queries
+	cin>>q;
+	for(int i=0;i<q;i++){
+	    int type;
+	    cin>>type;
+	    int l, r;
+	    cin>>l>>r;
+	    if(type == 1){
+	        update(l, r); // change the value at index l to r
+	    }
+	    else
+	    cout<<"Sum of elements in range ["<<l<<" - "<<r<<"] is "<<(query(r) - query(l-1))<<"\n";
+	}
+	return 0;
+}
+
