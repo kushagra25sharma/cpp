0e1aa2d4589622669f2a5932f938d7bcbfe3b9b6
Kushagra Sharma <69106317+kushagra25sharma@users.noreply.github.com>
2022-01-12 18:18:37 +0530

    Removed parent array and added comments

--- Bridge In Graph.cpp
+++ Bridge In Graph.cpp
@@ -5,20 +5,21 @@ int n;
 vector<int> v[200001];
 int currTime = 0;
 
-void dfs(int low[], int disc[], int vis[], int parent[], int vertex){
+void dfs(int low[], int disc[], int vis[], int parent, int vertex){
     currTime += 1;
     vis[vertex] = 1;
     disc[vertex] = low[vertex] = currTime;
     for(auto z : v[vertex]){
+        if(z == parent) continue; // if z == parent than it means we are going backwards or it's a cycle we need to move forward
         if(!vis[z]){
-            parent[z] = vertex;
-            dfs(low, disc, vis, parent, z);
-            low[vertex] = min(low[vertex], low[z]);
-            if(low[z] > disc[vertex]){
+            dfs(low, disc, vis, vertex, z);
+            low[vertex] = min(low[vertex], low[z]); // if there is a way to visit vertex earlier than current time.
+            if(low[z] > disc[vertex]){ // it means there is no other way to visit z. vertex has to be visited before we move to z
                 cout<<vertex<<" "<<z<<" -- ";
             }
         }
-        else if(parent[vertex] != z){
+        else{
+            // if z is previously visited then the lowest time will be the discovery time.
             low[vertex] = min(low[vertex], disc[z]);
         }
     }
@@ -39,7 +40,6 @@ int main(){
     int low[n] = {200001};
     int disc[n]= {0};
     int vis[n] = {0};
-    int parent[n] = {-1};
-    dfs(low, disc, vis, parent, 0);
+    dfs(low, disc, vis, -1, 0);
     return 0;
 }
